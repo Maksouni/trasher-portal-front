1beb16b63a6850696a1c7810c043caff7216a09f
Maksouni <max1205325@gmail.com>
2025-01-31 22:24:33 +0700

    excel report, dotenv.ts

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,22 +1,26 @@
 // src/api/axios.js
-import axios from 'axios';
-import Cookies from 'js-cookie';
+import axios from "axios";
+import Cookies from "js-cookie";
+import { apiUrl } from "../dotenv";
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: apiUrl,
 });
 
 // Добавляем токен в заголовки для каждого запроса
-instance.interceptors.request.use((config) => {
-  const token = Cookies.get('jwt_token')
-  
-  if (token) {
-    config.headers['Authorization'] = `Bearer ${token}`;
-  }
+instance.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get("jwt_token");
 
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
--- src/components/statistics/ChartBlock.tsx
+++ src/components/statistics/ChartBlock.tsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import { Download } from "@mui/icons-material";
+import { Button, Snackbar, Alert } from "@mui/material";
+import axios from "../../api/axios";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -7,8 +11,9 @@ import {
   Title,
   Tooltip,
   Legend,
-} from 'chart.js';
-import { Line } from 'react-chartjs-2';
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import { apiUrl } from "../../dotenv";
 
 ChartJS.register(
   CategoryScale,
@@ -20,43 +25,112 @@ ChartJS.register(
   Legend
 );
 
-interface ChartBlockProps{
-  title: string
+interface ChartBlockProps {
+  title: string;
 }
 
-export default function ChartBlock({title} : ChartBlockProps){
+export default function ChartBlock({ title }: ChartBlockProps) {
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
   const options = {
     responsive: true,
     plugins: {
       legend: {
-        position: 'bottom' as const,
+        position: "bottom" as const,
       },
     },
   };
-  
-  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  
+
+  const labels = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+  ];
+
   const data = {
     labels,
     datasets: [
       {
-        label: 'Обнаружения',
+        label: "Обнаружения",
         data: [65, 59, 80, 81, 56, 55, 40],
-        borderColor: 'rgb(75, 192, 192)',
-        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        borderColor: "rgb(75, 192, 192)",
+        backgroundColor: "rgba(75, 192, 192, 0.2)",
       },
       {
-        label: 'Точность',
+        label: "Точность",
         data: [45, 39, 60, 91, 76, 85, 60],
-        borderColor: 'rgb(255, 99, 132)',
-        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
       },
     ],
   };
-  return(
+
+  const downloadFile = async () => {
+    const date1 = new Date(2025, 0, 1).toISOString().split("T")[0];
+    const date2 = new Date(2025, 0, 15).toISOString().split("T")[0];
+
+    try {
+      const response = await axios.get(
+        `${apiUrl}/generate-report?date1=${date1}&date2=${date2}`,
+        {
+          responseType: "blob",
+        }
+      );
+
+      const blob = new Blob([response.data], {
+        type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", // MIME-тип Excel
+      });
+
+      const url = window.URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = "report.xlsx"; // Имя файла
+      document.body.appendChild(link);
+      link.click();
+
+      // Очистка
+      window.URL.revokeObjectURL(url);
+      document.body.removeChild(link);
+    } catch (error) {
+      console.error("Ошибка скачивания:", error);
+      setErrorMessage("Не удалось скачать отчёт. Попробуйте позже.");
+    }
+  };
+
+  return (
     <div className="chart-block">
       <h2>{title}</h2>
-      <Line options={options} data={data}/>
+      <Line options={options} data={data} />
+      <Button
+        type="button"
+        variant="contained"
+        color="success"
+        startIcon={<Download />}
+        sx={{ marginLeft: "auto", marginTop: "1rem" }}
+        onClick={downloadFile}
+      >
+        Скачать отчёт
+      </Button>
+
+      {/* Уведомление об ошибке */}
+      <Snackbar
+        open={!!errorMessage}
+        autoHideDuration={4000}
+        onClose={() => setErrorMessage(null)}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      >
+        <Alert
+          onClose={() => setErrorMessage(null)}
+          severity="error"
+          sx={{ width: "100%" }}
+        >
+          {errorMessage}
+        </Alert>
+      </Snackbar>
     </div>
-  )
+  );
 }
