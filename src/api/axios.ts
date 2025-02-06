e14c6f14ef2e3d7522d9073e332bec679b9718ee
maksouni <max1205325@gmail.com>
2025-02-06 13:24:02 +0700

    auth 401 response

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,24 +1,31 @@
 // src/api/axios.js
 import axios from "axios";
 import Cookies from "js-cookie";
-import { apiUrl } from "../dotenv";
 
 const instance = axios.create({
-  baseURL: apiUrl,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // Добавляем токен в заголовки для каждого запроса
 instance.interceptors.request.use(
   (config) => {
     const token = Cookies.get("jwt_token");
-
     if (token) {
       config.headers["Authorization"] = `Bearer ${token}`;
     }
-
     return config;
   },
+  (error) => Promise.reject(error)
+);
+
+// Обрабатываем ошибки ответов
+instance.interceptors.response.use(
+  (response) => response,
   (error) => {
+    if (error.response?.status === 401) {
+      Cookies.remove("jwt_token"); // Удаляем токен
+      window.location.href = "/login"; // Редирект на страницу логина
+    }
     return Promise.reject(error);
   }
 );
