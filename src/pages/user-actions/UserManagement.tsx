80bb6f0f66d64c0ed1c92567f81d98e5001b538b
Maksouni <max1205325@gmail.com>
2025-02-25 20:32:18 +0700

    makin user managment

--- src/pages/user-actions/UserManagement.tsx
+++ src/pages/user-actions/UserManagement.tsx
@@ -1,5 +1,21 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import {
+  Button,
+  Container,
+  InputAdornment,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+  Typography,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import AddIcon from "@mui/icons-material/Add";
 
 interface User {
   id: number;
@@ -14,10 +30,32 @@ const mockUsers: User[] = [
   { id: 3, username: "user2", password: "admin", role: "user" },
 ];
 
-// const roles = ["user", "admin"];
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+type Order = "asc" | "desc";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function getComparator<Key extends keyof any>(
+  order: Order,
+  orderBy: Key
+): (
+  a: { [key in Key]: number | string },
+  b: { [key in Key]: number | string }
+) => number {
+  return order === "desc"
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
 
 const UserManagement = () => {
-  // const [users, setUsers] = useState<User[]>(mockUsers);
   const users = mockUsers;
   const [searchQuery, setSearchQuery] = useState("");
   const navigate = useNavigate();
@@ -27,63 +65,11 @@ const UserManagement = () => {
   };
 
   return (
-    <div className="user-management-page">
-      {/* удалить потмо */}
-      <nav>
-        <Link
-          style={{
-            position: "fixed",
-            top: 2,
-            left: 8,
-          }}
-          to="/"
-        >
-          На главную
-        </Link>
-      </nav>
-      <div className="container">
-        <div className="users-page">
-          <h1>Управление пользователями</h1>
-          <div className="search-wrapper">
-            <input
-              type="email"
-              id="email"
-              value={searchQuery}
-              placeholder="Поиск пользователей"
-              maxLength={50}
-              onChange={(e) => setSearchQuery(e.target.value)}
-              className="search-input"
-            />
-            <button className="search-button">Найти</button>
-            <Link to="add">Добавить</Link>
-          </div>
-
-          <table>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Имя пользователя</th>
-                <th>Роль</th>
-                <th>Действие</th>
-              </tr>
-            </thead>
-            <tbody>
-              {users.map((user) => (
-                <tr key={user.id}>
-                  <td>{user.id}</td>
-                  <td>{user.username}</td>
-                  <td>{user.role}</td>
-                  <td>
-                    <button onClick={() => handleEdit(user.id)}>
-                      Изменить
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
+    <div
+      className="w-full max-w-[1024px] m-2 flex items-center justify-center
+          lg:mx-auto bg-white rounded-2xl shadow-lg"
+    >
+      <div>penis</div>
     </div>
   );
 };
