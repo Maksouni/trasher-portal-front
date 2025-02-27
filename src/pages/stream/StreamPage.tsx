3ead75a44be41f0988a09d93c74ea25c55037cd1
Maksouni <max1205325@gmail.com>
2025-02-27 20:55:16 +0700

    gitignore video, video mock

--- src/pages/stream/StreamPage.tsx
+++ src/pages/stream/StreamPage.tsx
@@ -1,63 +1,69 @@
-import { useRef, useEffect } from "react";
-import Hls from "hls.js";
-import Cookies from "js-cookie";
-import { apiUrl } from "../../dotenv";
+// import { useRef, useEffect } from "react";
+// import Hls from "hls.js";
+// import Cookies from "js-cookie";
+// import { apiUrl } from "../../dotenv";
 
 export default function StreamPage() {
-  const videoRef = useRef<HTMLVideoElement>(null);
-  const authToken = Cookies.get("jwt_token");
+  // const videoRef = useRef<HTMLVideoElement>(null);
+  // const authToken = Cookies.get("jwt_token");
 
-  useEffect(() => {
-    if (Hls.isSupported()) {
-      const hls = new Hls({
-        xhrSetup: (xhr) => {
-          if (authToken) {
-            xhr.setRequestHeader("Authorization", `Bearer ${authToken}`);
-          }
-        },
-      });
+  // useEffect(() => {
+  //   if (Hls.isSupported()) {
+  //     const hls = new Hls({
+  //       xhrSetup: (xhr) => {
+  //         if (authToken) {
+  //           xhr.setRequestHeader("Authorization", `Bearer ${authToken}`);
+  //         }
+  //       },
+  //     });
 
-      // Добавляем обработчик ошибок для диагностики
-      hls.on(Hls.Events.ERROR, (event, data) => {
-        console.error("HLS error:", event, data);
-      });
+  //     // Добавляем обработчик ошибок для диагностики
+  //     hls.on(Hls.Events.ERROR, (event, data) => {
+  //       console.error("HLS error:", event, data);
+  //     });
 
-      // Загружаем источник (ожидается m3u8 манифест)
-      hls.loadSource(`${apiUrl}media/stream`);
+  //     // Загружаем источник (ожидается m3u8 манифест)
+  //     hls.loadSource(`${apiUrl}media/stream`);
 
-      if (videoRef.current) {
-        hls.attachMedia(videoRef.current);
-        hls.on(Hls.Events.MANIFEST_PARSED, () => {
-          console.log("Manifest parsed, starting playback");
-          videoRef.current
-            ?.play()
-            .catch((err) =>
-              console.error("Ошибка воспроизведения видео:", err)
-            );
-        });
-      }
+  //     if (videoRef.current) {
+  //       hls.attachMedia(videoRef.current);
+  //       hls.on(Hls.Events.MANIFEST_PARSED, () => {
+  //         console.log("Manifest parsed, starting playback");
+  //         videoRef.current
+  //           ?.play()
+  //           .catch((err) =>
+  //             console.error("Ошибка воспроизведения видео:", err)
+  //           );
+  //       });
+  //     }
 
-      return () => {
-        hls.destroy();
-      };
-    } else if (
-      videoRef.current &&
-      videoRef.current.canPlayType("application/vnd.apple.mpegurl")
-    ) {
-      // Fallback для браузеров с нативной поддержкой HLS (например, Safari)
-      videoRef.current.src = `${apiUrl}media/stream`;
-      videoRef.current.addEventListener("loadedmetadata", () => {
-        videoRef.current
-          ?.play()
-          .catch((err) => console.error("Ошибка воспроизведения видео:", err));
-      });
-    }
-  }, [authToken]);
+  //     return () => {
+  //       hls.destroy();
+  //     };
+  //   } else if (
+  //     videoRef.current &&
+  //     videoRef.current.canPlayType("application/vnd.apple.mpegurl")
+  //   ) {
+  //     // Fallback для браузеров с нативной поддержкой HLS (например, Safari)
+  //     videoRef.current.src = `${apiUrl}media/stream`;
+  //     videoRef.current.addEventListener("loadedmetadata", () => {
+  //       videoRef.current
+  //         ?.play()
+  //         .catch((err) => console.error("Ошибка воспроизведения видео:", err));
+  //     });
+  //   }
+  // }, [authToken]);
 
   return (
     <div className="m-2 lg:mx-auto max-w-[1024px] bg-gray-600 rounded-2xl shadow-lg overflow-hidden">
       <div className="w-full h-fit min-h-[300px] flex items-center justify-center">
-        <video ref={videoRef} controls autoPlay muted className="w-full" />
+        <video
+          src="/home/portal/media/trash.mp4"
+          controls
+          autoPlay
+          muted
+          className="w-full"
+        />
       </div>
     </div>
   );
