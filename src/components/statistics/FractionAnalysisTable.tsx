6a6a8e72d4138c606270e7df73bde42c582060ee
maxouni <max1205325@gmail.com>
2025-07-30 22:35:29 +0700

    fetch data

--- src/components/statistics/FractionAnalysisTable.tsx
+++ src/components/statistics/FractionAnalysisTable.tsx
@@ -1,11 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useMemo, useState } from "react";
-import {
-  DataGrid,
-  GridColDef,
-  GridToolbarExport,
-  GridToolbarContainer,
-} from "@mui/x-data-grid";
+import { DataGrid, GridColDef, GridToolbarContainer } from "@mui/x-data-grid";
 import {
   MenuItem,
   ToggleButton,
