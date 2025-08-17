c39772f3ae667f10b38ba4671fe9ba9e08875873
maksouni <max1205325@gmail.com>
2025-08-18 01:11:33 +0700

    deploy fix

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 npm run build && \
-scp -r dist root@89.169.45.102:~/front_trash_test/ 
+scp -r dist root@89.169.45.102:/var/www/front_trash 
 # ssh -i ~/deploy_ssh root@217.28.223.212 "cd ~/front && screen -x front serve -s dist"
